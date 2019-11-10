0a6aa948c02a41087e88c18d0070a37b2ca5cebb
Yi EungJun <eungjun.yi@gmail.com>
2019-11-10 21:35:18 +0900

    한 줄 커버되지 않은 상태

---
+++ src/main/kotlin/im/toss/test/number.kt
@@ -0,0 +1,6 @@
+package im.toss.test
+
+fun number(target: Int) = when(target) {
+    1 -> "하나"
+    else -> "모름"
+}
---
+++ src/test/kotlin/im/toss/test/NumberKtTest.kt
@@ -0,0 +1,11 @@
+package im.toss.test
+
+import org.junit.jupiter.api.Test
+
+internal class TestKtTest {
+
+    @Test
+    fun hello() {
+        number(1).equalsTo("하나")
+    }
+}
